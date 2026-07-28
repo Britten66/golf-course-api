7c00a53affea1ed9bd1546ee3c1d1496a209f2f9
Chris <Britten63@hotmail.com>
2026-07-28 10:04:42 -0300

    chore: initial scaffold

---
+++ src/main/java/com/golfclub/api/GolfCourseApiApplication.java
@@ -0,0 +1,12 @@
+package com.golfclub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfCourseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfCourseApiApplication.class, args);
+    }
+}
